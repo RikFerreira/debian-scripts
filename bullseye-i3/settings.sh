6e020a08f328c1b60308329064b8a4948edc2b56
Rik Ferreira <rikferreiraalves@gmail.com>
2020-10-09 13:12:22 -0300

    Added external resources

--- bullseye-i3/settings.sh
+++ bullseye-i3/settings.sh
@@ -1,3 +1,5 @@
+echo -e "Configuração inicial"
+
 # Subdiretórios de /home
 echo -e "Subdiretórios de /home"
 mkdir "$HOME/docs"
@@ -10,6 +12,17 @@ mkdir -p "$HOME/.local/opt"
 mkdir -p "$HOME/.local/share"
 
 # Arquivos de configuração
+echo -e "Copiando arquivos de configuração"
 cp home/bash_profile "$HOME/.bash_profile"
+cp home/Xdefaults "$HOME/.Xdefaults"
+cp home/fehbg "$HOME/.fehbg"
+cp -R config "$HOME/.config"
+
+# Papéis de parede e fontes
+echo -e "Copiando papéis de parede"
+cp -R media/wallpapers "$HOME/.local/share"
+cp -R media/fonts "$HOME/.local/share"
+
+echo -e "Configuração inicial concluída!"
 
 exit 0
