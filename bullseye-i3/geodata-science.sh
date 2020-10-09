1d27aece565c0cfe77d714e8f11b54f0dd9196c8
RikFerreira <rikferreiraalves@gmail.com>
2020-10-09 19:25:19 -0300

    added rstudio installation to the script

--- bullseye-i3/geodata-science.sh
+++ bullseye-i3/geodata-science.sh
@@ -1,3 +1,9 @@
 sudo apt install r-base \
                  r-base-dev \
                  r-base-core
+
+wget https://download1.rstudio.org/desktop/bionic/amd64/rstudio-1.3.1093-amd64.deb -v
+
+sudo apt install "$HOME/rstudio-1.3.1093-amd64.deb" -f
+
+rm -f "$HOME/rstudio-1.3.1093-amd64.deb"
